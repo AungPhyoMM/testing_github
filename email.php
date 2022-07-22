149c3f840734814489f72ff97d53c916466dcf56
DESKTOP-GIHKKIL\DELL <aungphyoit.mm@gmail.com>
2022-07-22 22:14:13 +0630

    update email feature

--- email.php
+++ email.php
@@ -1,8 +1,4 @@
-<?php
-echo "sending email";
-echo "another email sent";
-echo "third line from cli";
-echo "third line from online";
-echo "fourth line from cli";
-echo "fourth line from online";
-echo "fifth line from online";
+VimCrypt~03!˜}Ü¯Ö“Îã_ÆšÖ$J?ó‘Ø5¼«‹ˆ|äÍ‡m’Þá¬g#½Á@“áˆÞýŽ”éiVŽ
+v¬€êbo
+¶KõLÖÈ%œGé ³y±áËÔ‹L	‘ ˜¨UMŒ'Á% ¯yŸbähâ-A,‘”/g˜ƒõE³¤œ*U-K]s,~ÛÄ>‚Å°šÁÙ€ˆU¸l9è±åÁyËxøSžŠ´kcÜËf&ti{Âeš‘òœzÎ¼X˜é/*‡7­<5cª¸yiÕ¸çk%Ð+@G)ÃÛ"Ü$MŠbÐ$
+#7ê¿´5˜‹O6æ	ay7š…Í^¤Ýð
